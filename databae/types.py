ce1174eb6edb6b0267f8251a641a3544fd67f29e
bubbleboy14 <mario.balibrera@gmail.com>
2025-10-22 04:30:42 -0700

    refactored (large chunks of) properties module into blob, keys, and types modules

---
+++ databae/types.py
@@ -0,0 +1,48 @@
+import sqlalchemy, json
+from .config import config
+
+primis = ["Integer", "Float", "Boolean", "Text", "Date", "Time"]
+
+class DynamicType(sqlalchemy.TypeDecorator):
+	cache_ok = config.cache
+
+	def __init__(self, *args, **kwargs):
+		self.choices = kwargs.pop("choices", None)
+		sqlalchemy.TypeDecorator.__init__(self, *args, **kwargs)
+
+class StringType(DynamicType):
+	def __init__(self, *args, **kwargs):
+		# len required for MySQL VARCHAR
+		DynamicType.__init__(self, kwargs.pop("length", config.stringsize), *args, **kwargs)
+
+def basicType(colClass, baseType=DynamicType):
+	cname = colClass.__name__
+	attrs = { "impl": colClass }
+	if config.cache and cname in primis:
+		attrs["cache_ok"] = True
+	return type("%s"%(cname,), (baseType,), attrs)
+
+BasicDT = basicType(sqlalchemy.DateTime)
+BasicString = basicType(sqlalchemy.VARCHAR, StringType)
+BasicText = basicType(sqlalchemy.UnicodeText)
+BasicInt = basicType(sqlalchemy.Integer)
+
+
+class DateTimeAutoStamper(BasicDT):
+	cache_ok = config.cache
+
+	def __init__(self, *args, **kwargs):
+		self.auto_now = kwargs.pop("auto_now", False)
+		self.auto_now_add = kwargs.pop("auto_now_add", False)
+		BasicDT.__init__(self, *args, **kwargs)
+
+	def should_stamp(self, is_new):
+		return self.auto_now or is_new and self.auto_now_add
+
+class JSONType(BasicText):
+	def process_bind_param(self, value, dialect):
+		return json.dumps(value)
+
+	def process_result_value(self, value, dialect):
+		return json.loads(value or "{}")
+
