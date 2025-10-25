b3b239446303bbfa530bf1c1ad8626e94101b651
bubbleboy14 <mario.balibrera@gmail.com>
2025-10-25 02:13:00 -0700

    composites - BasicText/BasicString Array/JSON variants

--- databae/composites.py
+++ databae/composites.py
@@ -1,26 +1,31 @@
 import json
-from .types import BasicText
+from .types import BasicString, BasicText
 from .keys import KeyWrapper
 from .config import config
 
-class JSONType(BasicText):
+class JSONBase(object):
 	def process_bind_param(self, value, dialect):
 		return json.dumps(value)
 
 	def process_result_value(self, value, dialect):
 		return json.loads(value or "{}")
 
-class ArrayType(BasicText):
+class JSONText(JSONBase, BasicText):
+	pass
+
+class JSONString(JSONBase, BasicString):
+	pass
+
+class ArrayBase(object):
 	cache_ok = config.cache
 
-	def __init__(self, *args, **kwargs):
+	def _keykinds(self, kwargs):
 		self.isKey = kwargs.pop("isKey", False)
 		if self.isKey:
 			self.kinds = kwargs.pop("kinds", [kwargs.pop("kind", "*")])
 			for i in range(len(self.kinds)):
 				if not isinstance(self.kinds[i], str):
 					self.kinds[i] = self.kinds[i].__name__.lower()
-		BasicText.__init__(self, *args, **kwargs)
 
 	def process_bind_param(self, value, dialect):
 		if self.isKey:
@@ -38,3 +43,16 @@ class ArrayType(BasicText):
 			for i in range(len(vlist)):
 				vlist[i] = KeyWrapper(vlist[i])
 		return vlist
+
+class ArrayText(ArrayBase, BasicText):
+	def __init__(self, *args, **kwargs):
+		self._keykinds(kwargs)
+		BasicText.__init__(self, *args, **kwargs)
+
+class ArrayString(ArrayBase, BasicString):
+	def __init__(self, *args, **kwargs):
+		self._keykinds(kwargs)
+		BasicString.__init__(self, *args, **kwargs)
+
+def ArrayType(**kwargs):
+	return (ArrayString if config.notext else ArrayText)(**kwargs)
