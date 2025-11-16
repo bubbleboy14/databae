29b9a85ad79ff1966e4ae99ec9283920ebc42fb7
bubbleboy14 <mario.balibrera@gmail.com>
2025-11-15 18:34:21 -0800

    vpush 0.1.4.16

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 DATabase ABstraction lAyEr
 
-v0.1.4.15
+v0.1.4.16
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='databae',
-    version="0.1.4.15",
+    version="0.1.4.16",
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
