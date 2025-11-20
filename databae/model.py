b96aa6033484000d6361bc15159a8e69d2a66fc0
bubbleboy14 <mario.balibrera@gmail.com>
2025-11-20 12:57:43 -0800

    moved ModelBase and TimeStampedBase to (new) poly module

--- databae/model.py
+++ databae/model.py
@@ -164,17 +164,3 @@ class FlatBase(ModelCore, metaclass=FlatMeta):
     @property
     def polytype(self):
         return self.__class__.__name__
-
-class ModelBase(ModelCore, metaclass=PolyMeta):
-    index = Integer(primary_key=True)
-    polytype = String()
-    key = CompositeKey()
-    __mapper_args__ = {
-        "polymorphic_on": polytype,
-        "polymorphic_identity": "modelbase",
-        "with_polymorphic": "*"
-    }
-
-class TimeStampedBase(ModelBase):
-    created = DateTime(auto_now_add=True)
-    modified = DateTime(auto_now=True)
